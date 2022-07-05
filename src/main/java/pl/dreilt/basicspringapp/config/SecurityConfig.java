b22beb4a960d5d10c6b9b3b8f7b628f64408a644
Dreilt <patrykdreilt@gmail.com>
2022-07-05 22:11:30 +0200

    Add home page and navbar

--- src/main/java/pl/dreilt/basicspringapp/config/SecurityConfig.java
+++ src/main/java/pl/dreilt/basicspringapp/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package pl.dreilt.basicspringapp.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -11,9 +12,17 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests(requests -> requests
+                .mvcMatchers("/").permitAll()
                 .anyRequest().authenticated());
         http.formLogin().permitAll();
         http.headers().frameOptions().disable();
         return http.build();
+    }
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return web -> web.ignoring().mvcMatchers(
+                "/styles/**"
+        );
     }
 }
