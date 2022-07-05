991312d22ecfcdb1a5d3bc5407d18e85470e8545
Dreilt <patrykdreilt@gmail.com>
2022-07-05 21:54:36 +0200

    App generated by Spring Initializr

---
+++ src/main/java/pl/dreilt/basicspringapp/BasicSpringAppApplication.java
@@ -0,0 +1,13 @@
+package pl.dreilt.basicspringapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicSpringAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BasicSpringAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/dreilt/basicspringapp/BasicSpringAppApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.dreilt.basicspringapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicSpringAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
