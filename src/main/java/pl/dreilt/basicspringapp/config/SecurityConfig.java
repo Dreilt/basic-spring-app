c708aa930aa1040eda1e72f5fa9d9d7ea6c02516
Dreilt <patrykdreilt@gmail.com>
2022-07-05 22:19:08 +0200

    Create custom login form

--- src/main/java/pl/dreilt/basicspringapp/config/SecurityConfig.java
+++ src/main/java/pl/dreilt/basicspringapp/config/SecurityConfig.java
@@ -2,9 +2,11 @@ package pl.dreilt.basicspringapp.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 public class SecurityConfig {
@@ -14,7 +16,12 @@ public class SecurityConfig {
         http.authorizeHttpRequests(requests -> requests
                 .mvcMatchers("/").permitAll()
                 .anyRequest().authenticated());
-        http.formLogin().permitAll();
+        http.formLogin(login -> login.loginPage("/login").permitAll());
+        http.logout(logout -> logout
+                .logoutRequestMatcher(new AntPathRequestMatcher("/logout/**", HttpMethod.GET.name()))
+                .logoutSuccessUrl("/login?logout").permitAll()
+        );
+        http.csrf().disable();
         http.headers().frameOptions().disable();
         return http.build();
     }
