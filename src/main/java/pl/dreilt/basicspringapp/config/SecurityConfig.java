34b28be62eb69e3b55ab0cdf1d507ceddc1a24f3
Dreilt <patrykdreilt@gmail.com>
2022-07-05 22:42:35 +0200

    Add Remember Me functionality

--- src/main/java/pl/dreilt/basicspringapp/config/SecurityConfig.java
+++ src/main/java/pl/dreilt/basicspringapp/config/SecurityConfig.java
@@ -6,10 +6,20 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.rememberme.JdbcTokenRepositoryImpl;
+import org.springframework.security.web.authentication.rememberme.PersistentTokenRepository;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class SecurityConfig {
+
+    private final DataSource dataSource;
+
+    public SecurityConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -21,6 +31,7 @@ public class SecurityConfig {
                 .logoutRequestMatcher(new AntPathRequestMatcher("/logout/**", HttpMethod.GET.name()))
                 .logoutSuccessUrl("/login?logout").permitAll()
         );
+        http.rememberMe().tokenRepository(persistentTokenRepository());
         http.csrf().disable();
         http.headers().frameOptions().disable();
         return http.build();
@@ -31,5 +42,12 @@ public class SecurityConfig {
         return web -> web.ignoring().mvcMatchers(
                 "/styles/**"
         );
+    }
+
+    @Bean
+    public PersistentTokenRepository persistentTokenRepository() {
+        JdbcTokenRepositoryImpl tokenRepository = new JdbcTokenRepositoryImpl();
+        tokenRepository.setDataSource(dataSource);
+        return tokenRepository;
     }
 }
